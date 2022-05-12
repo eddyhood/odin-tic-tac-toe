c756cf898259009024f1d0bc5361bc6cf9a72bf7
eddyhood <eddy@ignitespot.com>
2022-05-12 17:40:31 -0600

    Create starting view with game controller

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Modules
+function gameBoard(() {
+
+})();
+
+function gameControls(() {
+    const controlBox = document.getElementById('control-box');
+    
+
+})();
+
+//factories
+function createPlayer() {
+    return player;
+}
