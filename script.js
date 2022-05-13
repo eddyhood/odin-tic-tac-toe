41ae30e696e2888892802d890550c1b83ddf6dd6
eddyhood <eddy@ignitespot.com>
2022-05-13 16:11:34 -0600

    Fill game board with X's and O's
    Each click fills the game board with an alternating X or an O.

--- script.js
+++ script.js
@@ -79,3 +79,46 @@ function createPlayer(name) {
 
     return {name, gamesWon, turn}
 }
+
+(function playGame() {
+    const sq1 = document.getElementById('sq-1');
+    const sq2 = document.getElementById('sq-2');
+    const sq3 = document.getElementById('sq-3');
+    const sq4 = document.getElementById('sq-4');
+    const sq5 = document.getElementById('sq-5');
+    const sq6 = document.getElementById('sq-6');
+    const sq7 = document.getElementById('sq-7');
+    const sq8 = document.getElementById('sq-8');
+    const sq9 = document.getElementById('sq-9');
+
+    //Event listeners
+    sq1.addEventListener('click', e => getMove(e));
+    sq2.addEventListener('click', e => getMove(e));
+    sq3.addEventListener('click', e => getMove(e));
+    sq4.addEventListener('click', e => getMove(e));
+    sq5.addEventListener('click', e => getMove(e));
+    sq6.addEventListener('click', e => getMove(e));
+    sq7.addEventListener('click', e => getMove(e));
+    sq8.addEventListener('click', e => getMove(e));
+    sq9.addEventListener('click', e => getMove(e));
+
+    let move = 0;
+
+
+    function getMove(e) {
+        move += 1;
+        (move % 2 == 0) ? getWeapon(e,'P2') : getWeapon(e, 'P1');
+    };
+
+    function getWeapon(e, player) {
+        const weapon = document.createElement('i');
+
+        if (player == 'P1') {
+            weapon.classList.add('flaticon-close');
+        } else {
+            weapon.classList.add('flaticon-o');
+        }
+        e.target.appendChild(weapon);
+        return;
+    }    
+})();
